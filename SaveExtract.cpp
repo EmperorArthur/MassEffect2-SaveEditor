988d14a001b9a287ec888bb88bea434836f5a2f4
Arthur Moore <Arthur.Moore@cd-net.net>
2013-01-11 01:21:14 -0500

    Program can now output just the ME2 plot table.
    This can be expanded much further.

--- SaveExtract.cpp
+++ SaveExtract.cpp
@@ -10,23 +10,52 @@
 
 using namespace std;
 
+void printHelp(char* programName){
+	cout << "Usage:" << endl;
+	cout << "	" << programName << "[options] filename" << endl;
+	cout << endl;
+	cout << "Options are:"<<endl;
+	cout << "	-p		Display only the ME2 Plot table"<<endl;
+}
+
 int main(int argc, char *argv[]){
+	bool argumentP = false;
+	
+	//For each argument
+	for(int i = 1;i<(argc-1);i++){
+		//If we're passing in an option
+		if('-' == argv[i][0]){
+			if('p' == argv[i][1]){
+				argumentP = true;
+			}else{
+				cerr << "Unrecognized argument" << endl;
+				printHelp(argv[0]);
+			}
+		}
+	}
+	
 	//Make sure the filename is given
-	if(argc != 2){
+	if(argc < 2){
 		cerr << "ERROR:  Command incorrect" << endl;
 		cerr << "Usage:" << endl;
-		cerr << "	" << argv[0] << " file1" << endl;
+		cerr << "	" << argv[0] << "[options] filename" << endl;
 		return 1;
 	}
+	//Note, if a filename is not given, but at least one argument is:
+	//	Then the last argument is also treated as the file name in addition to its regular fuctions
 	
 	binFile file1;
 	ME2Format me2file;
-	cerr << "Extracting file: " << argv[1] << endl;
-	file1.open(argv[1]);
+	cerr << "Extracting file: " << argv[argc-1] << endl;
+	file1.open(argv[argc-1]);
 	cout << "File size is:  " << file1.size() << " bytes" << endl;
 	me2file.read(file1.fileStream);
-	file1.fileStream.close();	
-	me2file.cout();
+	file1.fileStream.close();
+	if(argumentP){
+		me2file.Plot.cout();
+	}else{
+		me2file.cout();
+	}
 	
 	return 0;
 }
