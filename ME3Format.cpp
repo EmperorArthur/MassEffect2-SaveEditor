21f9a4a931541b5a9cb1fb9aed09243f9162f815
Arthur Moore <Arthur.Moore@cd-net.net>
2013-01-14 05:30:41 -0500

    Began adding the Mass Effect 3 Save Format
    While I will probably split this off into a seperate repository in the future, for now I don't see a reason to.

---
+++ ME3Format.cpp
@@ -0,0 +1,4 @@
+//The save format used by Mass Effect 3
+//Copyright Arthur Moore 2012 GPLV3
+
+#include "ME3Format.hpp"
---
+++ ME3Format.hpp
@@ -0,0 +1,17 @@
+//The save format used by Mass Effect 3
+//Copyright Arthur Moore 2012 GPLV3
+
+#ifndef ME3FORMAT_H
+#define ME3FORMAT_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cassert>
+#include "BitArray.hpp"
+#include "collection.hpp"
+#include "mstring.hpp"
+
+
+
+#endif
