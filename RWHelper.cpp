aae8a29ca9f6ef4097959c26b1a57671099cf42a
Arthur Moore <Arthur.Moore@cd-net.net>
2013-01-17 03:59:35 -0500

    Created some helper functions so I no longer have to subclass string.
    I'd prefer to keep subclassing string, but my windows box stackdumps with the error in the kernel.
    The why and how is a mystery, but hopefully this will fix it.

---
+++ RWHelper.cpp
@@ -0,0 +1,22 @@
+//Helper functions for binary read write of strings
+//Copyright Arthur Moore 2012 GPL V3
+
+#include "RWHelper.hpp"
+
+void StringRead(fstream& saveFile,string& aString){
+	int readSize;
+	char readChar;
+	saveFile.read((char *) &readSize,4);
+	aString.clear();
+	if(0 == readSize){
+		return;
+	}
+	//This currently assumes that the readSize is correct
+	//I could look for the null terminator, but why bother.
+	for(int i=0;i< (readSize-1);i++){
+		saveFile.read(&readChar,1);
+		aString.append(1,readChar);
+	}
+	//Read and discard the null at the end of the string
+	saveFile.read(&readChar,1);
+}
